2f834944ecb170dbab3ebbe89e860432ad6b0701
lorenzo <lorenzo.softdev@gmail.com>
2023-10-29 19:41:37 +0100

    remove old hook

--- src/components/workout/modules/hooks/useFormMultiStep.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react';
-import FormMultiStepContext from '../contexts/form-multistep.context';
-
-const useForm = () => {
-  return useContext(FormMultiStepContext);
-};
-
-export default useForm;
