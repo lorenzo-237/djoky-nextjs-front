228700447d708211999e98a034e323d0d46af2b9
lorenzo <lorenzo.softdev@gmail.com>
2023-10-29 22:14:13 +0100

    replace /all by simply GET request

--- src/db/groups/fetch-groups.ts
+++ src/db/groups/fetch-groups.ts
@@ -1,7 +1,7 @@
 import { fetchApi } from '@/utils/fetch/server';
 
 export default async function fetchGroups(): Promise<GroupResponse | null> {
-  const response = await fetchApi('/groups/all', { cache: 'no-store' });
+  const response = await fetchApi('/groups', { cache: 'no-store' });
 
   if (!response.ok) return null;
 
