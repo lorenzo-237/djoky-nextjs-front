15628c70476affbbabab1b958fd29e83dd155976
lorenzo <lorenzo.softdev@gmail.com>
2023-10-29 17:54:14 +0100

    add initGroup (zustand) to data loading

--- src/components/data-loading.tsx
+++ src/components/data-loading.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useGroupStore } from '@/stores';
 import useCategoryStore from '@/stores/category.store';
 import { useEffect } from 'react';
 
@@ -12,13 +13,14 @@ type AppProps = { data: DataProps | null; children: JSX.Element };
 
 export const DataLoading = ({ data, children }: AppProps) => {
   const initCategory = useCategoryStore((state) => state.initData);
+  const initGroup = useGroupStore((state) => state.initData);
 
   useEffect(() => {
     console.log('\x1b[36m%s\x1b[0m', 'data fetched init datas');
     if (data && data.category) initCategory(data.category);
-    // setGroup(data?.group);
+    if (data && data.group) initGroup(data.group);
     // setExercise(data?.exercise);
-  }, [data, initCategory]);
+  }, [data, initCategory, initGroup]);
 
   return <>{children}</>;
 };
