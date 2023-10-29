d25132562b55d7b24ebbc07c2b2ee8ad4b60a637
lorenzo <lorenzo.softdev@gmail.com>
2023-10-29 17:29:42 +0100

    replace redux category slice by zustand store

--- src/stores/category.store.ts
+++ src/stores/category.store.ts
@@ -15,8 +15,44 @@ const initialState: CategoryState = {
 
 const useCategoryStore = create(
   combine(initialState, (set) => ({
-    initData: (payload: CategoryResponse) => set((state) => ({ ...state, response: payload })),
-    setCurrentUpdate: (dto: UpdateCategoryDto) => set((state) => ({ ...state, currentUpdate: dto })),
+    initData: (payload: CategoryResponse) => set(() => ({ response: payload })),
+    setCurrentUpdate: (dto: UpdateCategoryDto) => set(() => ({ currentUpdate: dto })),
+    validate: (categoryId: number) =>
+      set((state) => ({
+        response: {
+          ...state.response,
+          rows: state.response.rows.map((category) =>
+            category.id === categoryId ? { ...category, isPending: false } : category
+          ),
+        },
+      })),
+    pending: (categoryId: number) =>
+      set((state) => ({
+        response: {
+          ...state.response,
+          rows: state.response.rows.map((category) =>
+            category.id === categoryId ? { ...category, isPending: true } : category
+          ),
+        },
+      })),
+    add: (category: Category) =>
+      set((state) => ({
+        response: {
+          count: state.response.count + 1,
+          rows: [...state.response.rows, category],
+        },
+      })),
+    update: ({ id, name }: UpdateCategoryDto) =>
+      set((state) => ({
+        response: {
+          ...state.response,
+          rows: state.response.rows.map((category) => (category.id === id ? { ...category, name } : category)),
+        },
+        currentUpdate: {
+          id: 0,
+          name: '',
+        },
+      })),
   }))
 );
 
